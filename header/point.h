33b02e45bede162d17dded2f5409993a4102fa88
Chadi Hamrouni <69485266+ChadiHamrouni@users.noreply.github.com>
2023-06-04 14:25:16 +0100

    Update point.h

--- header/point.h
+++ header/point.h
@@ -4,15 +4,24 @@
 class Point {
 
 public:
-    float x = 0, y = 0, vel_x = 0, vel_y = 0, mass = 0, old_x = 0, old_y = 0;
+    float x = 0, y = 0;
+    float vel_x = 0, vel_y = 0;
+    float old_x = 0, old_y = 0;
+    float mass = 0;
     bool pinned = false;
+
     sf::CircleShape shape;
 
     Point(float x, float y, float mass, bool pinned);
+
     Point();
 
-    void Update_Verlet(float dt);
+    void UpdateVerlet(float dt);
 
     void Render(sf::RenderWindow& window);
+
+    static void SetInitialPosition(const int num_objects, std::vector<Point>& points);
+
+    static void SetAnchorPoints(std::vector<Point>& points);
 
 };
